a427145ba3d7bd4bc85624c8b636b062444d43ab
David Huber <david.huber@noaa.gov>
2022-05-03 18:20:13 +0000

    Improved the driver and report generator interface.

--- driver.sh
+++ driver.sh
@@ -1,12 +1,57 @@
 #!/usr/bin/bash
 set +x
+set -e
 module load miniconda/3.8-s4
+#PS4='Line ${LINENO}: '
+usage () {
+   echo "report_driver.sh [-y YY -m MM]"
+   echo "-y YY, 2-digit year"
+   echo "-m MM, 2-digit month"
+   echo "If a month and year are not supplied, stats for the previous month are generated"
+}
 
-set -x
-export y="22"
-export m="02"
+#set -x
+#Set defaults
+last_month=$(date -d "today-1 month" +%y/%m)
+export YY=$(echo $last_month | cut -b 1,2)
+export MM=$(echo $last_month | cut -b 4,5)
 
-echo Generating monthly job reports
-./gen_reports.sh -y $y -m $m >& gen_reports.out
-echo Generating monthly hours by job
+set +x
+[[ $# -eq 2 ]] && usage && exit 2
+while getopts ":y:m:h" opt; do
+   case $opt in
+      y)
+         YY=$OPTARG
+         ;;
+      m)
+         MM=$OPTARG
+         ;;
+      h)
+         usage
+         ;;
+      \?)
+         usage
+         echo "Invalid option: -$OPTARG"
+         exit 1
+         ;;
+      :)
+         usage
+         echo "Option -$OPTARG needs an argument."
+         ;;
+   esac
+done
+
+if [[ -z $YY || -z $MM ]]; then
+   usage
+   exit
+fi
+
+#set -x
+export y=$YY
+export m=$MM
+
+echo Generating monthly job reports.  This will take several minutes.
+./gen_reports.sh >& gen_reports.out
+echo Generating monthly hours by job.
 ./get_monthly_hours.sh >& get_monthly_hours.out
+echo Done
--- gen_reports.sh
+++ gen_reports.sh
@@ -2,55 +2,30 @@
 set +x
 set -e
 PS4='Line ${LINENO}: '
-usage () { echo "gen_reports.sh -y YY -m MM"; }
-[[ $# -lt 2 ]] && usage
-while getopts ":y:m:h" opt; do
-   case $opt in
-      y)
-         YY=$OPTARG
-         ;;
-      m)
-         MM=$OPTARG
-         ;;
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         echo "Invalid option: -$OPTARG"
-         exit 1
-         ;;
-      :)
-         usage
-         echo "Option -$OPTARG needs an argument."
-         ;;
-   esac
-done
 set -x
 
-export YYMM=$(echo "$YY$MM")
-export YY_MM=$(echo "${YY}_${MM}")
-export MM_YY=$(echo "${MM}_${YY}")
-export MMp1=$(( $MM + 1))
-export YYp1=$(date -d "20${YY}/${MM}/01+1 month" +%y)
+MM_YY=$(echo "${m}_${y}")
+MMp1=$(date -d "20$y/$m/01+1 month" +%m)
+YYp1=$(date -d "20$y/$m/01+1 month" +%y)
+
+start_YYYYMMDD="20${y}${m}01"
+end_YYYYMMDD="20${YYp1}${MMp1}01"
 
 YYYYMMDD_to_date() {
-   fYY=$(echo $1 | cut -b 1,4)
+   fYY=$(echo $1 | cut -b 1-4)
    fMM=$(echo $1 | cut -b 5,6)
    fDD=$(echo $1 | cut -b 7,8)
 
-   echo "20$fYY/$fMM/$fDD"
+   echo "$fYY/$fMM/$fDD"
 }
 
-sreport cluster UserUtilizationByAccount Format=Login%20,Accounts,Used Start=$MM/01/$YY End=$MMp1/01/$YYp1| grep -v -- "---" | sed "s/^  *//" | sed "s/   */,/g" > ActiveUsers_$MM_YY.csv
+sreport cluster UserUtilizationByAccount Format=Login%20,Accounts,Used Start=$m/01/$y End=$MMp1/01/$YYp1| grep -v -- "---" | sed "s/^  *//" | sed "s/   */,/g" > ActiveUsers_$MM_YY.csv
 
 
-export start_YYYYMMDD=$(echo "20${YY}${MM}01")
-export end_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $start_YYYYMMDD)+1 month" +%Y%m%d)
-export cur_YYYYMMDD=$start_YYYYMMDD
-export next_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $start_YYYYMMDD)+1 day" +%Y%m%d)
+cur_YYYYMMDD=$start_YYYYMMDD
+next_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $start_YYYYMMDD)+1 day" +%Y%m%d)
 
-echo "JobID,JobName,Partition,TotalCPU" > Jobs_${MM}_${YY}.csv
+echo "JobID,JobName,Partition,TotalCPU" > Jobs_${m}_${y}.csv
 while [ $cur_YYYYMMDD -lt $end_YYYYMMDD ]; do
    cur_YY=$(echo $cur_YYYYMMDD | cut -b 3,4)
    cur_MM=$(echo $cur_YYYYMMDD | cut -b 5,6)
@@ -61,11 +36,8 @@ while [ $cur_YYYYMMDD -lt $end_YYYYMMDD ]; do
    next_MM=$(echo $next_YYYYMMDD | cut -b 5,6)
    next_DD=$(echo $next_YYYYMMDD | cut -b 7,8)
    E=$next_MM$next_DD$next_YY
-   echo a$E-a s$S-s
-   sacct -S $S -E $E -a -o JobID%20,JobName%80,Partition,TotalCPU%20 | grep -v -- "---\|JobID" | sed -E "s/^  *(\S.*)  *$/\1/" | grep -Ev "^[0-9]*  *" | sed -E "s/,/;/g" | sed -E "s/^(\S*)  *(\S*)   *(\S*)$/\1,\2,,\3/" | grep -v " " >> Jobs_${MM}_$YY.csv
-   export cur_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $cur_YYYYMMDD)+1 day" +%Y%m%d)
-   export next_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $next_YYYYMMDD)+1 day" +%Y%m%d)
+
+   sacct -S $S -E $E -a -o JobID%20,JobName%80,Partition,TotalCPU%20 | grep -v -- "---\|JobID" | sed -E "s/^  *(\S.*)  *$/\1/" | grep -Ev "^[0-9]*  *" | sed -E "s/,/;/g" | sed -E "s/^(\S*)  *(\S*)   *(\S*)$/\1,\2,,\3/" | grep -v " " >> Jobs_${MM}_$y.csv
+   cur_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $cur_YYYYMMDD)+1 day" +%Y%m%d)
+   next_YYYYMMDD=$(date -d "$(YYYYMMDD_to_date $next_YYYYMMDD)+1 day" +%Y%m%d)
 done
-#echo "JobID,JobName,Partition,TotalCPU" > Jobs_04_21.csv; sacct -S 040121 -E 041021 -a -o JobID%20,JobName%80,Partition,TotalCPU%20 | grep -v -- "---" | sed "s/^  *//" | sed "s/  *$//" | sed -E "s/,/;/g" | sed -E "s/^(\S*)  *(\S*)   *(\S*)$/\1,\2,,\3/" |  sed -E "/^(\S*)  *(\S*)   *(\S*)  *(\S*)$/d" | grep -Ev "^[0-9]*  *" >> Jobs_04_21.csv
-#sacct -S 041021 -E 042021 -a -o JobID%20,JobName%80,Partition,TotalCPU%20 | grep -v -- "---" | sed "s/^  *//" | sed "s/  *$//" | sed -E "s/,/;/g" | sed -E "s/^(\S*)  *(\S*)   *(\S*)$/\1,\2,,\3/" |  sed -E "/^(\S*)  *(\S*)   *(\S*)  *(\S*)$/d" | grep -Ev "^[0-9]*  *" >> Jobs_04_21.csv
-#sacct -S 042021 -E 050121 -a -o JobID%20,JobName%80,Partition,TotalCPU%20 | grep -v -- "---" | sed "s/^  *//" | sed "s/  *$//" | sed -E "s/,/;/g" | sed -E "s/^(\S*)  *(\S*)   *(\S*)$/\1,\2,,\3/" |  sed -E "/^(\S*)  *(\S*)   *(\S*)  *(\S*)$/d" | grep -Ev "^[0-9]*  *" >> Jobs_04_21.csv
